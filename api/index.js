c19ea7339e93e9d94fa90fd51855be62647b113d
supratik <supratikpal53@gmail.com>
2024-02-26 12:06:27 +0530

    signup and signin created

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("db is connected")).catch((err) => {
+    console.log("db connection failed");
+})
+
+app.listen(3000, () => {
+    console.log("app is listening at port 3000")
+})
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
